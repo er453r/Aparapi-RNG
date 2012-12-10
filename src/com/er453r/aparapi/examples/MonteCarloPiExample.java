c8849ed85b279d76a0d9647fccbae951ec461095
Marcin Kotz <er453r@gmail.com>
2012-12-10 14:35:45 +0100

    better example

--- src/com/er453r/aparapi/examples/MonteCarloPiExample.java
+++ src/com/er453r/aparapi/examples/MonteCarloPiExample.java
@@ -5,35 +5,24 @@ import com.er453r.aparapi.rng.XORShiftKernel;
 
 public class MonteCarloPiExample{
 	public static void main(String[] _args){
-		final int threads = 512;
+		final int threads = 2 * 1024;
 		final int samplesPerPass = 1024;
-		final int passes = 512;
+		final int passes = 1024;
 
 		final long totalSamples = (long)threads * (long)samplesPerPass * (long)passes;
 				
-		final long hits[] = new long[]{0};
 		final int subsum[] = new int[threads];
 				
 		XORShiftKernel kernel = new XORShiftKernel(threads){
 			@Override public void run(){
-				if(getGlobalSize(0) > 1){
-					int gid = getGlobalId();
-					
-					for(int n = 0; n < samplesPerPass; n++){						
-						float x = randomn11();
-						float y = randomn11();
-	
-						if(x * x + y * y < 1)
-							subsum[gid]++;
-					}
-				}
-				else{					
-					long sum = 0;
-					
-					for(int n = 0; n < threads; n++)
-						sum += subsum[n];
-					
-					hits[0] = sum;
+				int gid = getGlobalId();
+				
+				for(int n = 0; n < samplesPerPass; n++){						
+					float x = randomn11();
+					float y = randomn11();
+
+					if(x * x + y * y < 1)
+						subsum[gid]++;
 				}
 			}
 		};
@@ -41,11 +30,15 @@ public class MonteCarloPiExample{
 		kernel.setExecutionMode(EXECUTION_MODE.GPU);
 				
 		kernel.execute(threads, passes);
-		kernel.execute(1);
 		
-		Double pi = 4 * hits[0] / (double) totalSamples;
+		long sum = 0;
 		
-		System.out.printf("pi = %1.20f, %,d hits from %,d samples\n", pi, hits[0], totalSamples);
+		for(int n = 0; n < threads; n++)
+			sum += subsum[n];
+		
+		Double pi = 4 * sum / (double) totalSamples;
+		
+		System.out.printf("pi = %1.20f, %,d hits from %,d samples\n", pi, sum, totalSamples);
 
 		System.out.printf("Execution mode = %s, time = %d ms.\n", kernel.getExecutionMode(), kernel.getAccumulatedExecutionTime());
 
